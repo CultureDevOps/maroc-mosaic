a9550d32f4395dcbac645f12bcb257e73da4bca6
CultureDevOps <chossat.sebastien@gmail.com>
2025-01-05 12:49:43 +0100

    Feat: optimize background

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -1,5 +1,6 @@
 import { createTranslation } from 'app/[locale]/i18n/server'
 import { LocaleTypes } from 'app/[locale]/i18n/settings'
+import Image from 'next/image'
 
 type Props = {
   params: { locale: LocaleTypes }
@@ -10,13 +11,17 @@ const heroBackgroundImg = '/static/images/pages/fontaine-02-enhanced.png'
 export default async function Hero({ params: { locale } }: Props) {
   const { t } = await createTranslation(locale, 'hero')
   return (
-    <div
-    className="relative bg-cover bg-center bg-no-repeat"
-    style={{
-      backgroundImage: `url(${heroBackgroundImg})`,
-      minHeight: '70vh',
-    }} 
-    >  
+    <div className="relative" style={{ minHeight: '70vh' }}>
+      {/* Image optimisée */}
+      <Image
+        src={heroBackgroundImg}
+        alt="Hero background"
+        layout="fill"
+        objectFit="cover"
+        quality={90}
+        priority
+        className="z-[-1] pointer-events-none" // Masque pour simuler un fond
+      />
     </div>
   )
 }
