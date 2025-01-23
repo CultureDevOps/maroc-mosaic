4bae0b11bbeb248f46c8a23614d1b8435e97f354
CultureDevOps <chossat.sebastien@gmail.com>
2025-01-23 18:10:30 +0100

    Fix: reenable GA

--- components/cookies/ConditionalAnalytics.tsx
+++ components/cookies/ConditionalAnalytics.tsx
@@ -1,37 +1,11 @@
 "use client"
 
 import React, { useEffect, useState } from "react"
+import { Analytics, AnalyticsConfig } from "pliny/analytics"
+import siteMetadata from "@/data/siteMetadata"
 import Cookies from "js-cookie"
-import ReactGA from "react-ga4"
 
 export default function ConditionalAnalytics() {
-  const [analyticsConsent, setAnalyticsConsent] = useState(
-    Cookies.get("analytics_consent") === "true"
-  )
-
-  useEffect(() => {
-    const handleConsentChange = () => {
-      setAnalyticsConsent(Cookies.get("analytics_consent") === "true")
-    }
-
-    // Ajoutez un écouteur pour surveiller les changements des cookies
-    window.addEventListener("cookieConsentChange", handleConsentChange)
-
-    return () => {
-      window.removeEventListener("cookieConsentChange", handleConsentChange)
-    }
-  }, [])
-
-  useEffect(() => {
-    if (analyticsConsent) {
-      ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY || "")
-      ReactGA.send({
-        hitType: "pageview",
-        page: window.location.pathname + window.location.search,
-      })
-    }
-  }, [analyticsConsent])
-
-  // Retourner null pour indiquer qu'aucun contenu n'est rendu
-  return null
+  const analyticsConsent = Cookies.get("analytics_consent") === "true"
+  return <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
 }
