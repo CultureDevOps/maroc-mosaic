074f0eae2ca574dc249afaceb9d6a0d345cbeec2
CultureDevOps <chossat.sebastien@gmail.com>
2025-01-23 17:14:04 +0100

    Feat: react-ga

--- components/cookies/ConditionalAnalytics.tsx
+++ components/cookies/ConditionalAnalytics.tsx
@@ -1,11 +1,26 @@
 "use client"
 
-import React, { useEffect, useState } from "react"
-import { Analytics, AnalyticsConfig } from "pliny/analytics"
+import React, { useEffect } from "react"
+import { AnalyticsConfig } from "pliny/analytics"
 import siteMetadata from "@/data/siteMetadata"
 import Cookies from "js-cookie"
+import ReactGA from "react-ga4"
 
 export default function ConditionalAnalytics() {
+  // Vérifie le consentement de l'utilisateur à l'utilisation des cookies d'analytics
   const analyticsConsent = Cookies.get("analytics_consent") === "true"
-  return <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
+
+  useEffect(() => {
+    if (analyticsConsent) {
+      ReactGA.initialize(process.env.NEXT_GOOGLE_ANALYTICS_KEY!)
+
+      ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search })
+    }
+  }, [analyticsConsent])
+
+  if (!analyticsConsent) {
+    return null
+  }
+
+  return null
 }
