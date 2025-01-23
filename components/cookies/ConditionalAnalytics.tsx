123bca187b75d63670ee8e885ab6c4b58345b2c0
CultureDevOps <chossat.sebastien@gmail.com>
2025-01-23 17:25:18 +0100

    Feat: GA

--- components/cookies/ConditionalAnalytics.tsx
+++ components/cookies/ConditionalAnalytics.tsx
@@ -1,26 +1,37 @@
 "use client"
 
-import React, { useEffect } from "react"
-import { AnalyticsConfig } from "pliny/analytics"
-import siteMetadata from "@/data/siteMetadata"
+import React, { useEffect, useState } from "react"
 import Cookies from "js-cookie"
 import ReactGA from "react-ga4"
 
 export default function ConditionalAnalytics() {
-  // Vérifie le consentement de l'utilisateur à l'utilisation des cookies d'analytics
-  const analyticsConsent = Cookies.get("analytics_consent") === "true"
+  const [analyticsConsent, setAnalyticsConsent] = useState(
+    Cookies.get("analytics_consent") === "true"
+  )
+
+  useEffect(() => {
+    const handleConsentChange = () => {
+      setAnalyticsConsent(Cookies.get("analytics_consent") === "true")
+    }
+
+    // Ajoutez un écouteur pour surveiller les changements des cookies
+    window.addEventListener("cookieConsentChange", handleConsentChange)
+
+    return () => {
+      window.removeEventListener("cookieConsentChange", handleConsentChange)
+    }
+  }, [])
 
   useEffect(() => {
     if (analyticsConsent) {
-      ReactGA.initialize(process.env.NEXT_GOOGLE_ANALYTICS_KEY!)
-
-      ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search })
+      ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY || "")
+      ReactGA.send({
+        hitType: "pageview",
+        page: window.location.pathname + window.location.search,
+      })
     }
   }, [analyticsConsent])
 
-  if (!analyticsConsent) {
-    return null
-  }
-
+  // Retourner null pour indiquer qu'aucun contenu n'est rendu
   return null
 }
