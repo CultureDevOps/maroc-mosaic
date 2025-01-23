c95809a9e76d869dfe2c7256885907bb1e1240ea
CultureDevOps <chossat.sebastien@gmail.com>
2025-01-23 23:15:25 +0100

    Fix: fancybox drag

--- components/mdxcomponents/FancyboxWrapper.tsx
+++ components/mdxcomponents/FancyboxWrapper.tsx
@@ -11,19 +11,6 @@ const FancyboxWrapper = ({ children }: { children: React.ReactNode }) => {
 
         // Associer Fancybox avec un gestionnaire d'événements
         Fancybox.bind("[data-fancybox='gallery']", {
-          trapFocus: false, // Désactive la gestion automatique du focus
-          autoFocus: false, // Empêche de donner le focus sur des éléments inutiles
-          placeFocusBack: true, // Retourne le focus à l'origine après fermeture
-          on: {
-            done: () => {
-              // Désactive le focus automatique et gère l'ouverture
-              document.body.style.pointerEvents = "none"
-            },
-            close: () => {
-              // Réactive les événements sur le body à la fermeture
-              document.body.style.pointerEvents = "auto"
-            },
-          },
           Toolbar: {
             display: {
               left: ["infobar"],
