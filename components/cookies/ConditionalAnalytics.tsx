5b1bbf67df407bc67ee0f01d14fd9e5f1be1ea44
CultureDevOps <chossat.sebastien@gmail.com>
2025-01-23 18:21:21 +0100

    Fix: conditional analytics

--- components/cookies/ConditionalAnalytics.tsx
+++ components/cookies/ConditionalAnalytics.tsx
@@ -6,6 +6,30 @@ import siteMetadata from "@/data/siteMetadata"
 import Cookies from "js-cookie"
 
 export default function ConditionalAnalytics() {
-  const analyticsConsent = Cookies.get("analytics_consent") === "true"
+  const [analyticsConsent, setAnalyticsConsent] = useState<boolean>(
+    Cookies.get("cookie_consent") === "true"
+  )
+
+  useEffect(() => {
+    const handleConsentChange = () => {
+      const consent = Cookies.get("cookie_consent") === "true"
+      setAnalyticsConsent(consent)
+    }
+
+    // Ajoutez un écouteur pour surveiller les changements des cookies ou un événement personnalisé
+    window.addEventListener("cookieConsentChange", handleConsentChange)
+
+    return () => {
+      window.removeEventListener("cookieConsentChange", handleConsentChange)
+    }
+  }, [])
+  console.log(Cookies.get("cookie_consent"))
+  console.log(`analyticsConsent : ${analyticsConsent}`)
+
+  // Si l'utilisateur a donné son consentement, afficher l'Analytics
+  if (!analyticsConsent) {
+    return null // Ou un message pour informer l'utilisateur de la nécessité d'accepter
+  }
+
   return <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
 }
